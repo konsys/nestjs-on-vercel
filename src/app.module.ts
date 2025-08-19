44fc749c275938c9314e5c00a22b3a08b9e25b71
Konstantin Sysuev <kasysuev@vtb.ru>
2025-08-19 16:33:40 +0300

    swefwsef

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,9 @@ console.log(dbPath)
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'db4free.net',
-      username: 'testtest12345',
-      password: 'testtest',
-      database: 'testtest12345',
+      username: 'testtest1234567',
+      password: 'testtest1234567',
+      database: 'testtest1234567',
       port: 3306,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
