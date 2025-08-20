5249fd848b79bea2dd41da21bb4ed583cb8658a0
Konstantin Sysuev <kasysuev@vtb.ru>
2025-08-20 10:28:58 +0300

    fix

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+const compression = require('compression');
+
+
+export async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+
+    app.enableCors({
+        exposedHeaders: '*',
+    });
+    app.use(compression());
+    await app.listen(process.env.PORT ?? 9000);
+}
+
