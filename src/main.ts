c65869d36ab4c74dad66ecfb012e8053e95e7566
Konstantin Sysuev <kasysuev@vtb.ru>
2025-08-21 13:33:16 +0300

    wsefwef

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,39 @@ import { AppModule } from './app.module';
 import { port } from './AppFactory';
 import compression from 'compression';
 
-export async function bootstrap() {
+// export async function bootstrap() {
+//     const app = await NestFactory.create(AppModule);
+
+
+//     app.enableCors({
+//         exposedHeaders: '*',
+//     });
+//     app.use(compression());
+//     await app.listen(process.env.PORT ?? port);
+// }
+
+
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import serverless = require('serverless-http');
+
+async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
-
+    const globalPrefix = '.netlify/functions/main';
     app.enableCors({
         exposedHeaders: '*',
     });
     app.use(compression());
-    await app.listen(process.env.PORT ?? port);
+
+    await app.init();
+
+    const expressApp = app.getHttpAdapter().getInstance();
+    return serverless(expressApp)
 }
 
+let server;
+export const handler = async (event, context, callback) => {
+    server = server ?? (await bootstrap());
+    return server(event, context, callback);
+};
